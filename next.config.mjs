8cedf46b07a106448d23d8982a6e14ce5d6551a0
Abayomi Akintomide <ywordk@gmail.com>
2024-07-08 00:20:18 -0400

    feat: instrumentation hook config

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 export default withMDX(nextConfig);
